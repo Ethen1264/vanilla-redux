db1008784c8282841a0bd5efd534b5f543ae87c5
Ethen1264 <jxorhks86909763@gmail.com>
2023-11-09 18:29:32 +0900

    add 3.3

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import { actionCreators } from '../store';
 
-function Home({toDos}) {
+function Home({toDos, addToDo}) {
   const [text, setText] = useState("")
   function onChange(e) {
     setText(e.target.value)
   }
   function onSubmit(e){
     e.preventDefault()
+    addToDo(text)
     setText("")
   }
   return (
@@ -25,7 +27,13 @@ function Home({toDos}) {
 }
 
 function mapStateToProps(state) {
-return {todOs: state}
+return {toDos: state}
 }
 
-export default connect(mapStateToProps)(Home);
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: (text) => dispatch(actionCreators.addToDo(text))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
